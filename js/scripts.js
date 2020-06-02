f5e11a5b268037ff5b160771e05dbf1176239e71
unknown <krzlepianka@gmail.com>
2020-06-03 01:36:55 +0200

    refactored grid, reponsive view. In Card section added different view based on width

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //ViewPort detection
-$( document ).ready(function() {
-    function scrollDetection() {
+window.addEventListener('DOMContentLoaded', (event) => {
+    /*function scrollDetection() {
         let aboutMeSection = document.querySelector('.about-me');
         let projectsSection = document.querySelector('.projects')
         let footerSection = document.querySelector('footer');
@@ -44,6 +44,64 @@ $( document ).ready(function() {
             }
         })
     }
-    scrollDetection();
+    scrollDetection();*/
+
+function checkInitialWidth() {
+    let navigationForLargeScreens = document.querySelector('.navigation');
+    let navigationForSmallScreens = document.querySelector('.navigation__mobile');
+    let currentWidth = window.innerWidth;
+    if(currentWidth <= 900) {
+        navigationForSmallScreens.style.display= 'block';
+        navigationForLargeScreens.style.display= 'none';
+    }
+    else {
+    navigationForSmallScreens.style.display= 'none';
+    navigationForLargeScreens.style.display= 'block';
+    }
+} 
+checkInitialWidth();
+
+
+function checkResize() { 
+    let navigationForLargeScreens = document.querySelector('.navigation');
+    let navigationForSmallScreens = document.querySelector('.navigation__mobile');
+    window.addEventListener('resize', function(e) {
+       let currentWidth = e.currentTarget.innerWidth;
+       if(currentWidth <= 900) {
+        navigationForSmallScreens.style.display= 'block';
+        navigationForLargeScreens.style.display= 'none';
+       }
+       else {
+        navigationForSmallScreens.style.display= 'none';
+        navigationForLargeScreens.style.display= 'block';
+       }
+   })
+}
+checkResize();
+
+
+function openMobileMenu() {
+    //const menu = document.querySelector('.navigation__menu');
+    const menu = document.querySelector('.navigation__container-btn');
+    const links = document.querySelector('.navigation__links-mobile');
+    let menuOpen = false;
+    menu.addEventListener('click', function() {
+        if(!menuOpen) {
+            menu.classList.add('open');
+            links.style.display ="block";
+            menuOpen = true;
+        }
+        else {
+            menu.classList.remove('open');
+            links.style.display = "none";
+            menuOpen = false;
+        }
+    })
+}
+
+openMobileMenu();
+
+
+
 });
 
